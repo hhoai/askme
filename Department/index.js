71cf254b7af96bab7f85b1506ca0a09b5596008b
hhoai <hhoaii261@gmail.com>
2023-11-29 18:10:23 +0700

    department

--- Department/index.js
+++ Department/index.js
@@ -1,5 +1,18 @@
-const items = document.querySelector(".department-content");
+const items = document.querySelector(".clicked");
+const newFile = document.querySelector(".new-file");
+const newDepartment = document.querySelector(".new-department");
 
 const handleDepartmentClicked = () => {
-  items.classList.toggle("isDisplay");
+  items.classList.add("active");
+  newFile.classList.remove("isDisplay");
+  newDepartment.classList.add("isDisplay");
 }
+
+const handleBackBtn = () => {
+  items.classList.remove("active");
+  newFile.classList.add("isDisplay");
+  newDepartment.classList.remove("isDisplay");
+}
+
+
+console.log(items);
